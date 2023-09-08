7cc5d02737bc47c5834b31fd351822290b764107
PritamKumarSaha <137249952+PritamKumarSaha@users.noreply.github.com>
2023-09-08 15:25:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0; i<str.length()-1; i++) {
+		if(str.indexOf(str.charAt(i) == str.lastIndexOf(str.charAt(i)))) {
+			return str.charAt(i);
+			break;
+		}
+		}
+		
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
